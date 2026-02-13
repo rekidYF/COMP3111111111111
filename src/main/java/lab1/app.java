3d98e717f8e6f50b0c7207258c86c4cbd5748430
rekid <yfanbh@connect.ust.hk>
2026-02-13 12:28:05 +0800

    I hate coding

---
+++ src/main/java/lab1/app.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class app {
+    public static void main(String[] args) {
+        int b = 2;
+        int n = 11;
+        System.out.println(b+" to power "+n+" = "+ lib.Power(b,n));
+        System.out.println(n+"! = " + lib.factorial(n));
+        System.out.println("Program ..Ended …");
+    }
+}
