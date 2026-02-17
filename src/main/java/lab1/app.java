dcb4ab82740372ce8b244634a5ab8c9b7e3f0280
rekid <yfanbh@connect.ust.hk>
2026-02-17 23:28:25 +0800

    I love hate coding

--- src/main/java/lab1/app.java
+++ src/main/java/lab1/app.java
@@ -1,7 +1,8 @@
 package lab1;
 
 public class app {
-    public static void main(String[] args) {
+    // hint: str args is never used. what if I delete it?
+    public static void main() {
         int b = 2;
         int n = 11;
         System.out.println(b+" to power "+n+" = "+ lib.Power(b,n));
